a5299f0e1f258a0d1659c790812d7191c89c2d33
mprahl <mprahl@users.noreply.github.com>
2020-08-16 16:07:31 -0400

    Add the initial database schema

---
+++ plant_wn/web/migrations/versions/c0f0926276e4_initial.py
@@ -0,0 +1,100 @@
+"""
+Initial migration.
+
+Revision ID: c0f0926276e4
+Revises:
+Create Date: 2020-08-16 17:23:39.762731
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = "c0f0926276e4"
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    op.create_table(
+        "max_precipitations",
+        sa.Column("id", sa.Integer(), nullable=False),
+        sa.Column("enabled", sa.Boolean(), nullable=False),
+        sa.Column("value", sa.Float(), nullable=False),
+        sa.PrimaryKeyConstraint("id"),
+    )
+    op.create_table(
+        "max_temps",
+        sa.Column("id", sa.Integer(), nullable=False),
+        sa.Column("enabled", sa.Boolean(), nullable=False),
+        sa.Column("value", sa.Float(), nullable=False),
+        sa.PrimaryKeyConstraint("id"),
+    )
+    op.create_table(
+        "max_winds",
+        sa.Column("id", sa.Integer(), nullable=False),
+        sa.Column("enabled", sa.Boolean(), nullable=False),
+        sa.Column("value", sa.Float(), nullable=False),
+        sa.PrimaryKeyConstraint("id"),
+    )
+    op.create_table(
+        "min_temps",
+        sa.Column("id", sa.Integer(), nullable=False),
+        sa.Column("enabled", sa.Boolean(), nullable=False),
+        sa.Column("value", sa.Float(), nullable=False),
+        sa.PrimaryKeyConstraint("id"),
+    )
+    op.create_table(
+        "users",
+        sa.Column("id", sa.Integer(), nullable=False),
+        sa.Column("username", sa.String(), nullable=False),
+        sa.Column("password", sa.String(), nullable=False),
+        sa.PrimaryKeyConstraint("id"),
+    )
+    with op.batch_alter_table("users", schema=None) as batch_op:
+        batch_op.create_index(batch_op.f("ix_users_username"), ["username"], unique=True)
+
+    op.create_table(
+        "zip_codes",
+        sa.Column("coordinates", sa.String(), nullable=True),
+        sa.Column("id", sa.Integer(), nullable=False),
+        sa.Column("zip_code", sa.String(), nullable=False),
+        sa.PrimaryKeyConstraint("id"),
+    )
+    with op.batch_alter_table("zip_codes", schema=None) as batch_op:
+        batch_op.create_index(batch_op.f("ix_zip_codes_zip_code"), ["zip_code"], unique=True)
+
+    op.create_table(
+        "plants",
+        sa.Column("id", sa.Integer(), nullable=False),
+        sa.Column("max_precipitation_id", sa.Integer(), nullable=True),
+        sa.Column("max_temp_id", sa.Integer(), nullable=True),
+        sa.Column("min_temp_id", sa.Integer(), nullable=True),
+        sa.Column("max_wind_id", sa.Integer(), nullable=True),
+        sa.Column("user_id", sa.Integer(), nullable=True),
+        sa.Column("zip_code_id", sa.Integer(), nullable=True),
+        sa.ForeignKeyConstraint(["max_precipitation_id"], ["max_precipitations.id"],),
+        sa.ForeignKeyConstraint(["max_temp_id"], ["max_temps.id"],),
+        sa.ForeignKeyConstraint(["max_wind_id"], ["max_winds.id"],),
+        sa.ForeignKeyConstraint(["min_temp_id"], ["min_temps.id"],),
+        sa.ForeignKeyConstraint(["user_id"], ["users.id"],),
+        sa.ForeignKeyConstraint(["zip_code_id"], ["zip_codes.id"],),
+        sa.PrimaryKeyConstraint("id"),
+    )
+
+
+def downgrade():
+    op.drop_table("plants")
+    with op.batch_alter_table("zip_codes", schema=None) as batch_op:
+        batch_op.drop_index(batch_op.f("ix_zip_codes_zip_code"))
+
+    op.drop_table("zip_codes")
+    with op.batch_alter_table("users", schema=None) as batch_op:
+        batch_op.drop_index(batch_op.f("ix_users_username"))
+
+    op.drop_table("users")
+    op.drop_table("min_temps")
+    op.drop_table("max_winds")
+    op.drop_table("max_temps")
+    op.drop_table("max_precipitations")
--- plant_wn/web/models.py
+++ plant_wn/web/models.py
@@ -1,1 +1,78 @@
 # SPDX-License-Identifier: GPL-3.0-or-later
+import sqlalchemy
+import sqlalchemy.orm
+
+from plant_wn.web import db
+
+
+class Threshold:
+    """A base class for a threshold."""
+
+    id = sqlalchemy.Column(sqlalchemy.Integer, primary_key=True)
+    enabled = sqlalchemy.Column(sqlalchemy.Boolean, nullable=False)
+    value = sqlalchemy.Column(sqlalchemy.Float, nullable=False)
+
+
+class MaxPrecipitation(db.Model, Threshold):
+    """A max precipitation threshold."""
+
+    __tablename__ = "max_precipitations"
+
+
+class MinTemp(db.Model, Threshold):
+    """A minimum temperature threshold."""
+
+    __tablename__ = "min_temps"
+
+
+class MaxTemp(db.Model, Threshold):
+    """A maximum temperature threshold."""
+
+    __tablename__ = "max_temps"
+
+
+class MaxWind(db.Model, Threshold):
+    """A maximum temperature threshold."""
+
+    __tablename__ = "max_winds"
+
+
+class Plant(db.Model):
+    """A plant that is tied to a location and thresholds."""
+
+    __tablename__ = "plants"
+    id = sqlalchemy.Column(sqlalchemy.Integer, primary_key=True)
+    max_precipitation_id = sqlalchemy.Column(
+        sqlalchemy.ForeignKey("max_precipitations.id"), nullable=True
+    )
+    max_temp_id = sqlalchemy.Column(sqlalchemy.ForeignKey("max_temps.id"), nullable=True)
+    min_temp_id = sqlalchemy.Column(sqlalchemy.ForeignKey("min_temps.id"), nullable=True)
+    max_wind_id = sqlalchemy.Column(sqlalchemy.ForeignKey("max_winds.id"), nullable=True)
+    user_id = sqlalchemy.Column(sqlalchemy.ForeignKey("users.id"))
+    zip_code_id = sqlalchemy.Column(sqlalchemy.ForeignKey("zip_codes.id"), nullable=True)
+
+    max_precipitation = sqlalchemy.orm.relationship("MaxPrecipitation", uselist=False)
+    max_temp = sqlalchemy.orm.relationship("MaxTemp", uselist=False)
+    min_temp = sqlalchemy.orm.relationship("MinTemp", uselist=False)
+    max_wind = sqlalchemy.orm.relationship("MaxWind", uselist=False)
+    user = sqlalchemy.orm.relationship("User", back_populates="plants")
+    zip_code = sqlalchemy.orm.relationship("ZipCode", uselist=False)
+
+
+class User(db.Model):
+    """A user that owns one or more plants."""
+
+    __tablename__ = "users"
+    id = sqlalchemy.Column(sqlalchemy.Integer, primary_key=True)
+    plants = sqlalchemy.orm.relationship("Plant", back_populates="user")
+    username = sqlalchemy.Column(sqlalchemy.String, nullable=False, index=True, unique=True)
+    password = sqlalchemy.Column(sqlalchemy.String, nullable=False)
+
+
+class ZipCode(db.Model):
+    """A zip code representing a location for a plant."""
+
+    __tablename__ = "zip_codes"
+    coordinates = sqlalchemy.Column(sqlalchemy.String)
+    id = sqlalchemy.Column(sqlalchemy.Integer, primary_key=True)
+    zip_code = sqlalchemy.Column(sqlalchemy.String, index=True, nullable=False, unique=True)
